1c7bf940ca7461403a2bed01498fe6ea1bd21180
Aman Chana <amanchana@Amans-MacBook-Air.local>
2022-02-12 10:04:33 -0500

    test fix

--- EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Annotations let java spring & gradle know that this is a spring boot application
+
 @RestController
 @SpringBootApplication
 public class EventRegistrationBackendApplication {
